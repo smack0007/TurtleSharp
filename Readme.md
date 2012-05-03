0b90f14b2c1eda576d3664f045e41c51612202cc
smack0007 <zachary.snow@gmail.com>
2012-05-03 11:38:51 +0200

    Added ScintillaNET for code editor and added Readme.md.

---
+++ Readme.md
@@ -0,0 +1,6 @@
+TurtleSharp
+===========
+
+TurtleSharp is an implementation of turtle graphics in C#.
+
+[http://en.wikipedia.org/wiki/Turtle_graphics]
--- TurtleSharp/Views/SourceCodeEditorControl.Designer.cs
+++ TurtleSharp/Views/SourceCodeEditorControl.Designer.cs
@@ -29,14 +29,15 @@
 		private void InitializeComponent()
 		{
 			this.splitContainer = new System.Windows.Forms.SplitContainer();
-			this.sourceCodeBox = new System.Windows.Forms.TextBox();
 			this.compilerErrorsView = new System.Windows.Forms.ListView();
 			this.descriptionHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
 			this.lineHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.sourceCodeBox = new ScintillaNET.Scintilla();
 			((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
 			this.splitContainer.Panel1.SuspendLayout();
 			this.splitContainer.Panel2.SuspendLayout();
 			this.splitContainer.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.sourceCodeBox)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// splitContainer
@@ -56,18 +57,6 @@
 			this.splitContainer.Size = new System.Drawing.Size(460, 379);
 			this.splitContainer.SplitterDistance = 244;
 			this.splitContainer.TabIndex = 0;
-			// 
-			// sourceCodeBox
-			// 
-			this.sourceCodeBox.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.sourceCodeBox.Font = new System.Drawing.Font("Segoe UI Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.sourceCodeBox.Location = new System.Drawing.Point(0, 0);
-			this.sourceCodeBox.Multiline = true;
-			this.sourceCodeBox.Name = "sourceCodeBox";
-			this.sourceCodeBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.sourceCodeBox.Size = new System.Drawing.Size(460, 244);
-			this.sourceCodeBox.TabIndex = 0;
-			this.sourceCodeBox.WordWrap = false;
 			// 
 			// compilerErrorsView
 			// 
@@ -91,6 +80,15 @@
 			// 
 			this.lineHeader.Text = "Line";
 			// 
+			// sourceCodeBox
+			// 
+			this.sourceCodeBox.ConfigurationManager.Language = "cs";
+			this.sourceCodeBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.sourceCodeBox.Location = new System.Drawing.Point(0, 0);
+			this.sourceCodeBox.Name = "sourceCodeBox";
+			this.sourceCodeBox.Size = new System.Drawing.Size(460, 244);
+			this.sourceCodeBox.TabIndex = 0;
+			// 
 			// SourceCodeEditorControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -99,10 +97,10 @@
 			this.Name = "SourceCodeEditorControl";
 			this.Size = new System.Drawing.Size(460, 379);
 			this.splitContainer.Panel1.ResumeLayout(false);
-			this.splitContainer.Panel1.PerformLayout();
 			this.splitContainer.Panel2.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
 			this.splitContainer.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.sourceCodeBox)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -110,9 +108,9 @@
 		#endregion
 
 		private System.Windows.Forms.SplitContainer splitContainer;
-		private System.Windows.Forms.TextBox sourceCodeBox;
 		private System.Windows.Forms.ListView compilerErrorsView;
 		private System.Windows.Forms.ColumnHeader descriptionHeader;
 		private System.Windows.Forms.ColumnHeader lineHeader;
+		private ScintillaNET.Scintilla sourceCodeBox;
 	}
 }
